713cdfc891efd81c8b742883fec59ba2e5aada20
roman.rolgin <ElNLj3Z3kDQfXs80FAZb>
2023-05-07 19:36:21 +0400

    inital structure + links / scss integration

---
+++ src/const.ts
@@ -0,0 +1,7 @@
+export const links = {
+  twitter: "https://twitter.com/rolginroman",
+  linkedin: "https://www.linkedin.com/in/rolgin-roman/",
+  github: "https://github.com/RolginRoman",
+  telegram: "https://t.me/itisngguru",
+  mail: "romanrolgin.fe@gmail.com",
+};
