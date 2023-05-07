aff196bdb4597267c3d477dc77e72f8e8317691f
roman.rolgin <ElNLj3Z3kDQfXs80FAZb>
2023-05-07 19:36:21 +0400

    inital structure + links / scss integration

---
+++ .prettierrc.cjs
@@ -0,0 +1,6 @@
+{
+  astroAllowShorthand: false;
+  printWidth: 120;
+  semi: true;
+  singleQuote: false;
+}
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  build: {
+    assets: "public",
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          includePaths: ["src/styles"],
+        },
+      },
+    },
+  },
+  experimental: {
+    inlineStylesheets: "auto",
+  },
+});
