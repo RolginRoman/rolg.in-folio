ab6a1324fb791f91cc720a6ee2fa3141213e470d
roman.rolgin <ElNLj3Z3kDQfXs80FAZb>
2023-05-09 05:52:30 +0400

    dark theme + css optimize + a lot of tuning

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rolginroman.github.io",
   base: "/rolg.in-folio",
   output: "static",
+  integrations: [purgecss()],
   build: {
     assets: "public",
   },
