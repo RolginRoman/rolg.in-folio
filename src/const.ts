43f5a95df42be986b91c63e4e17954d310b3d2e1
roman.rolgin <ElNLj3Z3kDQfXs80FAZb>
2023-05-27 12:54:05 +0400

    shortener

--- src/const.ts
+++ src/const.ts
@@ -1,8 +1,8 @@
 export const links = {
-  twitter: "https://twitter.com/rolginroman",
-  linkedin: "https://www.linkedin.com/in/rolgin-roman/",
-  github: "https://github.com/rolginroman",
-  telegram: "https://t.me/itisngguru",
+  twitter: "https://l.rolg.in/twitter",
+  linkedin: "https://l.rolg.in/linkedin",
+  github: "https://l.rolg.in/github",
+  telegram: "https://l.rolg.in/telegram",
   mail: "romanrolgin.fe@gmail.com",
-  cv: "https://drive.google.com/file/d/1vlCYT8bLncB5FLQ2YzjT0Zo3X1CrrVi2/view?usp=sharing",
+  cv: "https://l.rolg.in/cv",
 };
